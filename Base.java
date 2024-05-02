31fdd42884474bca21bbd2dd337fef01a02b1a9e
Sadashiv3131 <sadashivjadhav3131@gmail.com>
2024-05-02 08:24:11 +0530

    heloo

---
+++ Base.java
@@ -0,0 +1,15 @@
+public class Base {
+    public int A;
+    public int B;
+    public int C;
+    {
+        System.out.println("Inside Base constructor");
+        this.A = 11;
+        this.B = 21;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
